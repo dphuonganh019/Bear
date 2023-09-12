9afd2bd35be3c8ce6a1f7d0f1d9ca0d4451e00a6
Dell 7490 <dphuonganh019@gmail.com>
2023-09-12 18:23:09 +0700

    Update Bài 1 Circle PRO

--- src/bai1Circle/Circle.java
+++ src/bai1Circle/Circle.java
@@ -2,17 +2,21 @@ package bai1Circle;
 
 public class Circle {
     private double radius =1.0;
-    private String color = "red";
+    private String color = "Red";
     public Circle(){
         //Không tham số
     }
-    public Circle(double radius){
+    public Circle(double radius,String color){
         this.radius = radius;
+        this.color = color;
     }
     public double getRadius(){
         return radius;
     }
     public double getArea(){
         return Math.PI * radius * radius;
+    }
+    public String getColor(){
+        return color;
     }
 }
--- src/bai1Circle/TestCircle.java
+++ src/bai1Circle/TestCircle.java
@@ -5,9 +5,11 @@ public class TestCircle {
         Circle circle1 = new Circle();
         System.out.println("Bán kính của Circle1 là: " + circle1.getRadius());
         System.out.println("Diện tích của Circle1 là: " + circle1.getArea());
+        System.out.println("Màu sắc của Circle1 là: " + circle1.getColor());
 
-        Circle circle2 = new Circle(5.5);
+        Circle circle2 = new Circle(5.5,"blue");
         System.out.println("Bán kính của Circle2 là: " + circle2.getRadius());
         System.out.println("Diện tích của Circle2 là: " + circle2.getArea());
+        System.out.println("Màu sắc của Circle2 là: " + circle2.getColor());
     }
 }
